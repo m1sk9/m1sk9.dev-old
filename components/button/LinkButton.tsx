17509b5b4bdc55d60e5b3f2fcba3aa52aa3aaaa0
lis2a <me@lilis.dev>
2023-07-05 02:47:20 +0900

    feat: LinkButton component

--- components/button/LinkButton.tsx
+++ components/button/LinkButton.tsx
@@ -2,29 +2,31 @@ import { Button } from "@chakra-ui/react";
 
 type Props = {
   icon: React.ReactElement;
-  href: string;
-  color: string;
+  domain: string;
+  id: string;
+  color?: string;
   disabled?: boolean;
 };
 
 const LinkButton: React.FC<Props> = ({
   icon,
-  href,
+  domain,
+  id,
   color,
   disabled = false,
 }) => {
   return (
     <Button
-      className={"hover:text-gray-600"}
       as={"a"}
-      colorScheme={color}
       variant={"link"}
-      href={href}
+      href={"https://" + domain + "/" + id}
       leftIcon={icon}
       target={"_blank"}
       rel={"noopener noreferrer"}
       disabled={disabled}
-    />
+    >
+      {id}
+    </Button>
   );
 };
 
