9476bb8735347d5d5cd3af4dddb0728a817f72d1
lis2a <me@lilis.dev>
2023-07-05 00:54:50 +0900

    refactor!: deprecated color theme

--- components/button/ToggleThemeButton.tsx
+++ components/button/ToggleThemeButton.tsx
@@ -2,6 +2,7 @@
 
 import { Button, Tooltip } from "@mantine/core";
 
+// NOTE: 当初はテーマ切り替えを実装しようとしていたが、面倒になったので実装していない
 function ToggleThemeButton() {
   return (
     <Tooltip label={"テーマ切り替え"}>
@@ -18,7 +19,11 @@ function ToggleThemeButton() {
 }
 
 function ToggleTheme() {
-  if (document.documentElement.classList.contains("dark")) {
+  const themeStorageStore = localStorage.theme;
+  if (
+    document.documentElement.classList.contains("dark") ||
+    themeStorageStore === "dark"
+  ) {
     document.documentElement.classList.remove("dark");
     localStorage.theme = "light";
   } else {
