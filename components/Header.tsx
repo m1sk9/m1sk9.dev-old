1e6289253e5ee878314a66f080b4f5795ada6825
m1sk9 <me@m1sk9.dev>
2023-09-08 20:55:22 +0900

    feat: update display header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FaGithub, FaKeybase, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaTwitter } from "react-icons/fa";
 import Image from "next/image";
 import LinkButton from "./button/LinkButton";
 import { Stack } from "@chakra-ui/react";
@@ -13,8 +13,7 @@ const Header = () => {
       }
     >
       <Image src={"https://github.com/m1sk9.png"} alt={"m1sk9のアイコン"} width={200} height={200} />
-      <p className={"mt-5 text-4xl font-bold"}>める</p>
-      <p className={"mt-2 text-2xl"}>m1sk9</p>
+      <p className={"mt-5 text-4xl font-bold"}>m1sk9</p>
 
       <Stack className={"text-1xs mt-3"} direction={"row"} spacing={12}>
         <LinkButton
@@ -27,7 +26,6 @@ const Header = () => {
           domain={"twitter.com"}
           id={"m1sk22"}
         />
-        <LinkButton icon={<FaKeybase />} domain={"keybase.io"} id={"m1sk9"} />
       </Stack>
     </div>
   );
